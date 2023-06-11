ba121bc5ea15f48b383521f64df55cff725ed72d
Ahmed <ahmed.m.almousawi@gmail.com>
2023-06-11 17:32:00 -0600

    labelled data

--- init.py
+++ init.py
@@ -12,5 +12,9 @@ config_path = 'C:\\Users\\ahmed\\Desktop\\HeartValidation--DeepLabCut\\DLC Test-
 if os.path.exists('DLC Test-BMERIT - Ahmed Almousawi-2023-06-11') == False:
     config_path = deeplabcut.create_new_project('DLC Test', 'BMERIT - Ahmed Almousawi', videolist, working_directory=cwd, copy_videos=False, multianimal=False)
 
-deeplabcut.extract_frames(config_path, mode='automatic', algo='kmeans', userfeedback=False)
+# deeplabcut.extract_frames(config_path, mode='automatic', algo='kmeans', userfeedback=False)
+
+# deeplabcut.check_labels(config_path, visualizeindividuals=True)
+
+deeplabcut.SkeletonBuilder(config_path)
 
