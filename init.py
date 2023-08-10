424e0ca7f0051a6b92300726560180d8128fc247
Ahmed <ahmed.m.almousawi@gmail.com>
2023-08-10 00:09:16 -0600

    labeled and trained and skeleton

--- init.py
+++ init.py
@@ -3,12 +3,12 @@ import deeplabcut, os
 cwd = os.getcwd()
 print(f'Successfully imported DeepLabCut version: {deeplabcut. __version__}')
 
-videolist = ['Videos\HeartBaseline.mp4', 'Videos\HeartUltrasound.mp4']
+videolist = ['Videos\HeartUltrasound.mp4']
 for i in enumerate(videolist):
     videolist[i[0]] = cwd + '\\' + videolist[i[0]]
 print(videolist)
 
-config_path = 'C:\\Users\\ahmed\\Desktop\\HeartValidation--DeepLabCut\\DLC Test-BMERIT - Ahmed Almousawi-2023-06-11\\config.yaml'
+config_path = 'C:\\Users\\Ahmed\\Desktop\\HeartValidation--DeepLabCut\\Ultrasound1-Ahmed-2023-08-09\\config.yaml'
 if os.path.exists('DLC Test-BMERIT - Ahmed Almousawi-2023-06-11') == False:
     config_path = deeplabcut.create_new_project('DLC Test', 'BMERIT - Ahmed Almousawi', videolist, working_directory=cwd, copy_videos=False, multianimal=False)
 
